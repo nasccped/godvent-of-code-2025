5d810c7b30df3d3fcdfd75d840e746ea8c9b3329
Nasccped <pedro.main.tec@gmail.com>
2025-12-05 00:09:15 +0000

    chore: add byte digit to integer conversion

--- internal/extramath/extramath.go
+++ internal/extramath/extramath.go
@@ -1,5 +1,7 @@
 package extramath
 
+import "fmt"
+
 // Returns the absolute value of an integer (stdlib provides only
 // `abs` of `float64`).
 func AbsInt(value int) int {
@@ -7,4 +9,20 @@ func AbsInt(value int) int {
 		value *= -1
 	}
 	return value
+}
+
+// Converts a numeric byte (`0-9`) into an integer. Can return an
+// error if the provided byte isn't contained within the numeric
+// range.
+func NumByteToInt(b byte) (int, error) {
+	var (
+		i   int
+		err error
+	)
+	if b >= '0' && b <= '9' {
+		i = int(b - '0')
+	} else {
+		err = fmt.Errorf("the provided byte isn't a valid digit (`%c`)", b)
+	}
+	return i, err
 }
